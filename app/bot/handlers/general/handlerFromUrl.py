0b7057d2fca05e6593d66dcf386c5d34bd816ae7
Oleksandr Nychyk <nychyk.oleksandr@lll.kpi.ua>
2023-11-23 21:13:26 +0200

    starting fromurl

--- app/bot/handlers/general/handlerFromUrl.py
+++ app/bot/handlers/general/handlerFromUrl.py
@@ -1,1 +1,7 @@
 import aiohttp
+# import validator
+# from aiogram.types import Message
+# # from app.messages.start_message import START
+# from app.messages.uploadmessages import FILENAME_MSG
+# from app.bot.keyboard.downloadkb import download_Keyboard
+
